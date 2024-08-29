b1f4843bd32cfbe88abaa2a77001fe6b6cf2025a
wesam-c <wesameldeeb15@gmail.com>
2024-08-30 02:53:40 +0400

    add

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,31 @@
+import logo from './holberton_logo.jpg';
+import './App.css';
+import React from 'react';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button>OK</button>
+        </form>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
